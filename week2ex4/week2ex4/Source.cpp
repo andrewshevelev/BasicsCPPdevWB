3c74aa1e8ef1beb9bbbef89834c149dc3a176c1f
Andrew Shevelev <andrew.shevelev@gmail.com>
2017-08-25 11:33:32 +0700

    week2ex4

---
+++ week2ex4/week2ex4/Source.cpp
@@ -0,0 +1,28 @@
+/*
+* Напишите функцию UpdateIfGreater, которая принимает
+* два целочисленных аргумента: first и second.
+* Если first оказался больше second, Ваша функция должна
+* записывать в second значение параметра first. При этом
+* изменение параметра second должно быть видно на вызывающей стороне.
+*/
+
+//#include <iostream>
+//using namespace std;
+
+void UpdateIfGreater(const int& first, int& second) {
+	if (first > second) {
+		second = first;
+	}
+}
+
+
+//int main() {
+//	int a = 4;
+//	int b = 2;
+//	UpdateIfGreater(a, b);
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//
+//
+//	return 0;
+//}

208b8cd39d5134a2e5d16746f0318eda83cf7339
Andrew Shevelev <andrew.shevelev@gmail.com>
2017-09-07 21:42:06 +0700

    week4ex10

---
+++ week4ex10/week4ex10/Source.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+
+class Rational {
+public:
+	Rational() {
+		numerator = 0;
+		denominator = 1;
+	}
+	Rational(int numerator_, int denominator_) {
+		if (denominator_ == 0) {
+			throw invalid_argument("denominator must not be 0");
+		}
+		else if (numerator_ == 0) {
+			numerator = 0;
+			denominator = 1;
+		}
+		else
+		{
+			int flag_sign = 1;
+			if (numerator_ < 0 && denominator_ < 0) {
+				numerator_ = (-1) * numerator_;
+				denominator_ = (-1) * denominator_;
+			}
+			else if (numerator_ < 0 && denominator_ > 0) {
+				numerator_ = (-1) * numerator_;
+				flag_sign = -1;
+			}
+			else if (numerator_ > 0 && denominator_ < 0) {
+				denominator_ = (-1) * denominator_;
+				flag_sign = -1;
+			}
+
+			int nod = NOD(numerator_, denominator_);
+			numerator = flag_sign*numerator_ / nod;
+			denominator = denominator_ / nod;
+		}
+	}
+
+	int Numerator() const {
+		return numerator;
+	}
+
+	int Denominator() const {
+		return denominator;
+	}
+
+private:
+	int numerator;
+	int denominator;
+	int NOD(int a, int b) {
+		while (a > 0 && b > 0) {
+			if (a > b) {
+				a %= b;
+			}
+			else {
+				b %= a;
+			}
+		}
+		return a + b;
+	}
+};
+
+bool operator==(const Rational& lhs, const Rational& rhs) {
+	if (lhs.Denominator() == rhs.Denominator() &&
+		lhs.Numerator() == rhs.Numerator()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+Rational operator+(const Rational& lhs, const Rational& rhs) {
+	return Rational(lhs.Numerator()*rhs.Denominator() + rhs.Numerator()*lhs.Denominator(),
+		lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator-(const Rational& lhs, const Rational& rhs) {
+	return Rational(lhs.Numerator()*rhs.Denominator() - rhs.Numerator()*lhs.Denominator(),
+		lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator*(const Rational& lhs, const Rational& rhs) {
+	return Rational(lhs.Numerator() * rhs.Numerator(),
+		lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator/(const Rational& lhs, const Rational& rhs) {
+	if (rhs.Numerator() == 0) {
+		throw domain_error("domain error");
+	}	
+	return Rational(lhs.Numerator() * rhs.Denominator(),
+		lhs.Denominator() * rhs.Numerator());
+}
+
+ostream& operator<<(ostream& output, const Rational& rat) {
+	output << rat.Numerator() << "/" << rat.Denominator();
+	return output;
+}
+
+istream& operator>>(istream& input, Rational& rat) {
+	int numerator;
+	int denominator;
+	if (input >> numerator && input.ignore(1) && input >> denominator) {
+		rat = Rational(numerator, denominator);
+	}
+	return input;
+}
+
+bool operator<(const Rational& lhs, const Rational& rhs) {
+	return (lhs.Numerator()*rhs.Denominator() < rhs.Numerator()*lhs.Denominator());
+}
+
+int main() {
+	try {
+		Rational r(1, 0);
+		cout << "Doesn't throw in case of zero denominator" << endl;
+		return 1;
+	}
+	catch (invalid_argument& ex) {
+		cout << ex.what() << endl;
+	}
+
+	try {
+		auto x = Rational(1, 2) / Rational(0, 1);
+		cout << "Doesn't throw in case of division by zero" << endl;
+		return 2;
+	}
+	catch (domain_error&) {
+	}
+
+	cout << "OK" << endl;
+	return 0;
+}
